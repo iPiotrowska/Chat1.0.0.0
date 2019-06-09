54365784f697655dd35ac1f55e9ba46bf1a44f47
Joanna Bielińska <joan.bielinska@gmail.com>
2019-06-09 18:17:04 +0200

    Added hamburger

--- Chat1.0.0.0/MainWindow.xaml.cs
+++ Chat1.0.0.0/MainWindow.xaml.cs
@@ -9,20 +9,36 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Chat1._0._0._0
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        bool StateClosed = true;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (StateClosed)
+            {
+                Storyboard sb = this.FindResource("OpenMenu") as Storyboard;
+                sb.Begin();
+            }
+            else
+            {
+                Storyboard sb = this.FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+            }
+
+            StateClosed = !StateClosed;
         }
     }
 }
