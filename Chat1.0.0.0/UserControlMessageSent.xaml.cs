47630f76c1f76b745ae6e24d12bf32ba6657b6a3
Joanna Bielińska <joan.bielinska@gmail.com>
2019-06-20 22:11:18 +0200

    Totally changed everything

---
+++ Chat1.0.0.0/UserControlMessageSent.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Chat1._0._0._0
+{
+    public partial class UserControlMessageSent : UserControl
+    {
+        public UserControlMessageSent()
+        {
+            InitializeComponent();
+        }
+    }
+}
