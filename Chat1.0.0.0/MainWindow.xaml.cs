a80e0ff3af92a18268b14bcff12cf6735255f64b
Joanna Bielińska <joan.bielinska@gmail.com>
2019-06-23 18:08:19 +0200

    Added social media hyperlink

--- Chat1.0.0.0/MainWindow.xaml.cs
+++ Chat1.0.0.0/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,32 @@ namespace Chat1._0._0._0
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<UserControlMessageSent> sentMessages = new List<UserControlMessageSent>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Sent_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlMessageSent sentMessage = new UserControlMessageSent();
+            sentMessage.Message = textBoxMessage.Text;
+            //sentMessages.Add(sentMessage);
+            ListOfMessages.Children.Add(sentMessage);
+            textBoxMessage.Clear();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
--- Chat1.0.0.0/UserControlMessageSent.xaml.cs
+++ Chat1.0.0.0/UserControlMessageSent.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace Chat1._0._0._0
 {
     public partial class UserControlMessageSent : UserControl
     {
+        public string Message { get; set; }
+
         public UserControlMessageSent()
         {
             InitializeComponent();
